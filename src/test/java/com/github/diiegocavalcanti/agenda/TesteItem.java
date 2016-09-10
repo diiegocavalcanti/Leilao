3309bca1b6ca644760b895110ea550768aef600f
Diego Cavalcanti <diego@agenciamoob.com.br>
2016-09-10 14:41:30 -0300

    Atualização dos Testes

--- src/test/java/com/github/diiegocavalcanti/agenda/TesteItem.java
+++ src/test/java/com/github/diiegocavalcanti/agenda/TesteItem.java
@@ -11,4 +11,9 @@ public class TesteItem {
 		Leilao l = new Leilao(new Usuario("Diego Cavalcanti", "diego@email.com"),"Notebook HP", "Hotebook HP Com 8Gb de Memória, 500gb de HD");
 		assertEquals("Diego Cavalcanti", l.dono().nome());
 	}
+	@Test
+	public void deveTerUmEmail(){
+		Leilao l = new Leilao(new Usuario("Diego Cavalcanti", "diego@email.com"),"Notebook HP", "Hotebook HP Com 8Gb de Memória, 500gb de HD");
+		assertEquals("diego@email.com", l.dono().email());
+	}
 }
